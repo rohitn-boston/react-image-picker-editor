69b89257894a35fc26989d34a1939c7c6e99a7cd
LAPTOP-ROHITH-N\Rohith <rohith>
2023-11-06 13:22:07 +0530

    updated types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -8,7 +8,7 @@ declare module '*.mp4' {
     const src: string;
     export default src;
   }
-  declare module '*.PNG' {
+  declare module '*.png' {
     const src: string;
     export default src;
   }
